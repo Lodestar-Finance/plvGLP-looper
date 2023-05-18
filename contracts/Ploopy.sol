5b6ff0fdba5792d09a8c6a1cf8777440ac41709e
Coopes <cooperlimond@gmail.com>
2023-05-17 23:00:39 -0500

    testing lplvglp transfer

--- contracts/Ploopy.sol
+++ contracts/Ploopy.sol
@@ -8,18 +8,18 @@ import './PloopyConstants.sol';
 
 contract Ploopy is IPloopy, PloopyConstants, Ownable, IFlashLoanRecipient, ReentrancyGuard {
   constructor() {
-    // // approve rewardRouter to spend USDC for minting GLP
-    // USDC.approve(address(REWARD_ROUTER_V2), type(uint256).max);
-    // USDC.approve(address(GLP), type(uint256).max);
-    // USDC.approve(address(VAULT), type(uint256).max);
-    // USDC.approve(address(GLP_MANAGER), type(uint256).max);
-    // // approve GlpDepositor to spend GLP for minting plvGLP
-    // sGLP.approve(address(GLP_DEPOSITOR), type(uint256).max);
-    // GLP.approve(address(GLP_DEPOSITOR), type(uint256).max);
-    // sGLP.approve(address(REWARD_ROUTER_V2), type(uint256).max);
-    // GLP.approve(address(REWARD_ROUTER_V2), type(uint256).max);
-    // // approve lPLVGLP to spend plvGLP to mint lPLVGLP
-    // PLVGLP.approve(address(lPLVGLP), type(uint256).max);
+    // approve rewardRouter to spend USDC for minting GLP
+    USDC.approve(address(REWARD_ROUTER_V2), type(uint256).max);
+    USDC.approve(address(GLP), type(uint256).max);
+    USDC.approve(address(VAULT), type(uint256).max);
+    USDC.approve(address(GLP_MANAGER), type(uint256).max);
+    // approve GlpDepositor to spend GLP for minting plvGLP
+    sGLP.approve(address(GLP_DEPOSITOR), type(uint256).max);
+    GLP.approve(address(GLP_DEPOSITOR), type(uint256).max);
+    sGLP.approve(address(REWARD_ROUTER_V2), type(uint256).max);
+    GLP.approve(address(REWARD_ROUTER_V2), type(uint256).max);
+    // approve lPLVGLP to spend plvGLP to mint lPLVGLP
+    PLVGLP.approve(address(lPLVGLP), type(uint256).max);
   }
 
   // Declare events
@@ -28,15 +28,16 @@ contract Ploopy is IPloopy, PloopyConstants, Ownable, IFlashLoanRecipient, Reent
   event BalanceOf(uint256 balanceAmount, uint256 loanAmount);
   event Allowance(uint256 allowance, uint256 loanAmount);
   event UserDataEvent(address indexed from, uint256 plvGlpAmount, string indexed borrowedToken, uint256 borrowedAmount);
+  event PLVGLPBalanceChange(uint256 priorBalanceAmount, uint256 newBalanceAmount);
+  event lPLVGLPBalanceChange(uint256 newBalanceAmount);
 
   function loop(uint256 _plvGlpAmount, uint16 _leverage) external {
-  require(tx.origin == msg.sender, "Not an EOA");
-  require(_plvGlpAmount > 0, "Amount must be greater than 0");
-  require(_leverage >= DIVISOR && _leverage <= MAX_LEVERAGE, "Invalid leverage");
+    require(tx.origin == msg.sender, "Not an EOA");
+    require(_plvGlpAmount > 0, "Amount must be greater than 0");
+    require(_leverage >= DIVISOR && _leverage <= MAX_LEVERAGE, "Invalid leverage");
 
     // Transfer plvGLP to this contract so we can mint in 1 go.
     PLVGLP.transferFrom(msg.sender, address(this), _plvGlpAmount);
-    emit Transfer(msg.sender, address(this), _plvGlpAmount);
 
     uint256 loanAmount = getNotionalLoanAmountIn1e18(
       _plvGlpAmount * PRICE_ORACLE.getPlvGLPPrice(),
@@ -96,20 +97,26 @@ contract Ploopy is IPloopy, PloopyConstants, Ownable, IFlashLoanRecipient, Reent
 
     // TODO whitelist this contract for plvGLP mint
     // mint plvGLP. Approval needed.
-    uint256 _oldPlvglpBal = PLVGLP.balanceOf(address(this));
-    GLP_DEPOSITOR.deposit(glpAmount);
-    uint256 _newPlvglpBal = PLVGLP.balanceOf(address(this));
-    require(_newPlvglpBal > _oldPlvglpBal, "GLP deposit failed");
+    // uint256 _oldPlvglpBal = PLVGLP.balanceOf(address(this));
+    // GLP_DEPOSITOR.deposit(glpAmount);
+
+    // // check new balances and confirm we properly minted
+    // uint256 _newPlvglpBal = PLVGLP.balanceOf(address(this));
+    // require(_newPlvglpBal > _oldPlvglpBal, "GLP deposit failed");
+    // emit PLVGLPBalanceChange(_oldPlvglpBal, _newPlvglpBal);
 
     // mint lPLVGLP by depositing plvGLP. Approval needed.
     unchecked {
-      uint256 mintedFromBorrow = lPLVGLP.mint(PLVGLP.balanceOf(address(this)) - _oldPlvglpBal);
-      //TODO: store the amount minted from borrow, so we can potentially unwind this without fees
+      // mint lplvGLP
+      lPLVGLP.mint(PLVGLP.balanceOf(address(this)));
 
-      uint256 mintedFromUser = lPLVGLP.mint(data.plvGlpAmount);
+      // pull our most up to date lplvGLP balance
+      uint256 newlPlvglpBal = lPLVGLP.balanceOf(address(this));
+      emit lPLVGLPBalanceChange(newlPlvglpBal);
 
       // transfer lPLVGLP minted to user
-      lPLVGLP.transfer(data.user, mintedFromBorrow + mintedFromUser);
+      lPLVGLP.transfer(data.user, newlPlvglpBal);
+      emit Transfer(msg.sender, data.user, newlPlvglpBal);
     }
 
     // call borrowBehalf to borrow USDC on behalf of user
